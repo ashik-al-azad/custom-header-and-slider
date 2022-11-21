13b0d367de37d30b05528b2965dcee59b2081ea7
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-21 23:11:00 +0600

    Add files via upload

---
+++ Class-38-cerousel-slide/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+ 
+  $('.owl-carousel').owlCarousel({
+    dots: true,
+    items: 1,
+    autoplay: true,
+    loop: true,
+    nav: true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+  })
+ 
+});
+
